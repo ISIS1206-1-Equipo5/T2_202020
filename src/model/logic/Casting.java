d5465694ed0ce60d66fafa85f52359e0160d4218
CarlosRamirezM <c.ramirezm@uniandes.edu.co>
2020-08-22 22:47:55 -0500

    Implementacion inicial

--- src/model/logic/Casting.java
+++ src/model/logic/Casting.java
@@ -1,14 +1,89 @@
 package model.logic;
 
-import com.opencsv.bean.CsvBindByPosition;
-
 public class Casting {
-
-	@CsvBindByPosition(position = 0)
-	public String id;
 	
-	public String getId() {
+	public final static int NOT_AVAILABLE = 0;
+	
+	public final static int WOMEN = 1;
+	
+	public final static int MALE = 2;
+	
+	private int id;
+	
+	private String producerName;
+	
+	private int producerNumber;
+	
+	private String screenplayName;
+	
+	private String editorName;
+	
+	private int actorNumber;
+	
+	private int directorNumber;
+	
+	private Actor[] actors;
+	
+	private Director director;
+	
+	public Casting(int pId, String pProducerName, int pProducerNumber, String pScreenPlayName, String pEditorName, int pActorNumber, int pDirectorNumber)
+	{
+		id = pId;
+		producerName = pProducerName;
+		producerNumber = pProducerNumber;
+		directorNumber = pDirectorNumber;
+		actorNumber = pActorNumber;
+		directorNumber = pDirectorNumber;
+		actors = new Actor[5];
+		director = null;
+		
+	}
+	
+	public void addActor(String pName, int pGender, int i) throws IndexOutOfBoundsException
+	{		
+		Actor toAdd = new Actor(pName, pGender);
+		
+		actors[i] = toAdd;
+	}
+	
+	public int getId()
+	{
 		return id;
+	}
+	
+	public String getProducerName()
+	{
+		return producerName;
+	}
+	
+	public int getProducerNumber()
+	{
+		return producerNumber;
+	}
+	
+	public String getScreenPlayName()
+	{
+		return screenplayName;
+	}
+	
+	public String getEditorName()
+	{
+		return editorName;
+	}
+	
+	public int getActorNumber()
+	{
+		return actorNumber;
+	}
+	
+	public Actor[] getActors()
+	{
+		return actors;
+	}
+	
+	public Director getDirector()
+	{
+		return director;
 	}
 
 }
