9281e930e93c7ea659233301c4593f8ddc0fd899
CarlosRamirezM <c.ramirezm@uniandes.edu.co>
2020-08-22 22:48:32 -0500

    Implementacion clase Director

--- src/model/logic/Director.java
+++ src/model/logic/Director.java
@@ -1,5 +1,26 @@
 package model.logic;
 
 public class Director {
+	
+private String name;
+	
+	private int gender;
+	
+	public Director(String pName, int pGender)
+	{
+		name = pName;
+		
+		gender = pGender;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getGender()
+	{
+		return gender;
+	}	
 
 }
