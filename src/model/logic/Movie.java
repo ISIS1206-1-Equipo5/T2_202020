ed859df3a693f84ecaaeeec99fafafce700a613b
CarlosRamirezM <c.ramirezm@uniandes.edu.co>
2020-08-23 00:32:23 -0500

    Implementacion compareTo() y @

--- src/model/logic/Movie.java
+++ src/model/logic/Movie.java
@@ -1,52 +1,78 @@
 package model.logic;
 
+import com.opencsv.bean.CsvBindByPosition;
+
 public class Movie implements Comparable<Movie>{
 	
+	@CsvBindByPosition(position = 0)
 	private int id;
 	
+	@CsvBindByPosition(position = 1)
 	private int budget;
+
+	@CsvBindByPosition(position = 2)
+	private String genres;
 	
+	@CsvBindByPosition(position = 3)
 	private String imbd_id;
 	
+	@CsvBindByPosition(position = 4)
 	private String originalLanguage;
 	
+	@CsvBindByPosition(position = 5)
 	private String originalTitle;
 	
+	@CsvBindByPosition(position = 6)
 	private String overview;
 	
+	@CsvBindByPosition(position = 7)
 	private double popularity;
 	
+	@CsvBindByPosition(position = 8)
 	private String productionCompanies;
 	
+	@CsvBindByPosition(position = 9)
 	private String productionCountries;
 	
+	@CsvBindByPosition(position = 10)
 	private String releaseDate;
 	
+	@CsvBindByPosition(position = 11)
 	private int revenue;
 	
+	@CsvBindByPosition(position = 12)
 	private String status;
 	
+	@CsvBindByPosition(position = 13)
 	private String tagline;
 	
+	@CsvBindByPosition(position = 14)
 	private String title;
 	
+	@CsvBindByPosition(position = 15)
 	private double voteAverage;
 	
+	@CsvBindByPosition(position = 16)
 	private int voteCount;
 	
+	@CsvBindByPosition(position = 17)
 	private int productionCompaniesNumber;
 	
+	@CsvBindByPosition(position = 18)
 	private int productionCountriesNumber;
 	
+	@CsvBindByPosition(position = 19)
 	private int spokenLanguagesNumber;
 	
+	@CsvBindByPosition(position = 20)
 	private Casting casting;
 	
 	
-	public Movie(int pId, int pBudget, String pImbd_id, String pOriginalLanguage, String pOrigninalTitle, String pOverview, double pPopularity, String pProductionCompanies, String pProductionCountries, String pReleaseDate, int pRevenue, String pStatus, String pTagline, String pTitle, double pVoteAverage, int pVoteDiscount, int pPCompNumber, int pPCountNumber, int pSpokenLanNumber)
+	public Movie(int pId, int pBudget, String pGenres, String pImbd_id, String pOriginalLanguage, String pOrigninalTitle, String pOverview, double pPopularity, String pProductionCompanies, String pProductionCountries, String pReleaseDate, int pRevenue, String pStatus, String pTagline, String pTitle, double pVoteAverage, int pVoteDiscount, int pPCompNumber, int pPCountNumber, int pSpokenLanNumber)
 	{
 		id = pId;
 		budget = pBudget;
+		genres = pGenres;
 		imbd_id = pImbd_id;
 		originalLanguage = pOriginalLanguage;
 		originalTitle = pOrigninalTitle;
@@ -74,6 +100,18 @@ public class Movie implements Comparable<Movie>{
 	public int getBudget()
 	{
 		return budget;
+	}
+	
+	public String getGenres()
+	{
+		return genres;
+	}
+	
+	public String[] getListGenres()
+	{
+		String[] listGenres = genres.split("|");
+		
+		return listGenres;
 	}
 	
 	public String getImbd_id()
@@ -172,9 +210,14 @@ public class Movie implements Comparable<Movie>{
 	}
 
 	@Override
-	public int compareTo(Movie o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Movie o) 
+	{
+		if(this.voteAverage < o.getVoteAverage())
+			return -1;
+		else if(this.voteAverage > o.getVoteAverage())
+			return 1;
+		else 
+			return 0;				
 	}
 	
 
